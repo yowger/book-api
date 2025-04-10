10f3bbd08f2d764b58ca9950b30fd991c98642d6
Ver0018 <rogerpantilyowger@gmail.com>
2025-04-11 05:33:40 +0800

    added create, get by id category endpoint, sayonra

--- BookStore.Api/Endpoints/CategoryEndpoints.cs
+++ BookStore.Api/Endpoints/CategoryEndpoints.cs
@@ -1,12 +1,25 @@
+using BookStore.Api.Entities;
 using BookStore.Api.Interfaces;
 
 namespace BookStore.Api.Endpoints;
 
 public static class CategoryEndpoints
 {
+    const string routeName = "categories";
+    const string getCategoryByIdEndpointName = "GetCategoryById";
+
     public static void MapCategoryEndpoints(this IEndpointRouteBuilder routes)
     {
-        var categoriesGroup = routes.MapGroup("/categories");
+        var categoriesGroup = routes.MapGroup($"/{routeName}");
+
+        categoriesGroup.MapPost("/", async (Category category, ICategoryRepository categoryRepository) =>
+        {
+            // TODO: add find by name, return conflict
+
+            await categoryRepository.AddAsync(category);
+
+            return Results.Created($"/{routeName}/{category.Id}", category);
+        });
 
         categoriesGroup.MapGet("/", async (ICategoryRepository categoryRepository) =>
         {
@@ -14,5 +27,19 @@ public static class CategoryEndpoints
 
             return Results.Ok(categories);
         });
+
+        categoriesGroup.MapGet("/{id}", async (Guid id, ICategoryRepository categoryRepository) =>
+        {
+            var category = await categoryRepository.GetByIdAsync(id);
+
+            if (category is null)
+            {
+                return Results.NotFound();
+            }
+
+            return Results.Ok(category);
+        }).WithName(getCategoryByIdEndpointName);
+
+
     }
 }
