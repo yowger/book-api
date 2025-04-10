f4519be29d6a55827904a72dfdd136793c2a9880
Ver0018 <rogerpantilyowger@gmail.com>
2025-04-11 04:48:21 +0800

    added ef category repo

---
+++ BookStore.Api/Repositories/EntityFrameworkCategoryRepository.cs
@@ -0,0 +1,48 @@
+using BookStore.Api.Data;
+using BookStore.Api.Entities;
+using BookStore.Api.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.Api.Repositories;
+
+public class EntityFrameworkCategoryRepository : ICategoryInterface
+{
+    private readonly BookStoreContext _context;
+
+    public EntityFrameworkCategoryRepository(BookStoreContext context)
+    {
+        _context = context;
+    }
+
+    public async Task AddAsync(Category category)
+    {
+        await _context.Categories.AddAsync(category);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task<IEnumerable<Category>> GetAllAsync()
+    {
+        return await _context.Categories.ToListAsync();
+    }
+
+    public async Task<Category?> GetByIdAsync(Guid id)
+    {
+        return await _context.Categories.FindAsync(id);
+    }
+
+    public async Task UpdateAsync(Category category)
+    {
+        _context.Categories.Update(category);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(Guid id)
+    {
+        var category = await GetByIdAsync(id);
+        if (category != null)
+        {
+            _context.Categories.Remove(category);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
