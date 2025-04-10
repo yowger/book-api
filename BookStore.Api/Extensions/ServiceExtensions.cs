a4e7edaa23caa715401e798e4e01b575e72751cd
Ver0018 <rogerpantilyowger@gmail.com>
2025-04-11 03:49:21 +0800

    updated database config, refactored added services extension

---
+++ BookStore.Api/Extensions/ServiceExtensions.cs
@@ -0,0 +1,49 @@
+using BookStore.Api.Data;
+using BookStore.Api.Interfaces;
+using BookStore.Api.Repositories;
+using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.Api.Extensions;
+
+public static class ServiceExtensions
+{
+    public static IServiceCollection AddDatabaseContext(this IServiceCollection services, IConfiguration configuration, IWebHostEnvironment environment)
+    {
+        var connectionString = configuration.GetConnectionString("BookStoreContext")
+            ?? throw new InvalidOperationException("Connection string 'BookStoreContext' not found.");
+        var serverVersion = new MySqlServerVersion(new Version(8, 0, 40));
+
+        services.AddDbContext<BookStoreContext>(options =>
+        {
+            options.UseMySql(connectionString, serverVersion, mysqlOptions =>
+            {
+                mysqlOptions.EnableRetryOnFailure(
+                    maxRetryCount: 3,
+                    maxRetryDelay: TimeSpan.FromSeconds(30),
+                    errorNumbersToAdd: null
+                );
+            });
+
+            if (environment.IsDevelopment())
+            {
+                options
+                    .LogTo(Console.WriteLine, LogLevel.Information)
+                    .EnableSensitiveDataLogging()
+                    .EnableDetailedErrors();
+            }
+        });
+
+
+        return services;
+    }
+
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        services.AddValidatorsFromAssembly(typeof(Program).Assembly, includeInternalTypes: true);
+        services.AddOpenApi();
+        services.AddSingleton<IBookRepository, InMemoryBooksRepository>();
+
+        return services;
+    }
+}
--- BookStore.Api/Program.cs
+++ BookStore.Api/Program.cs
@@ -1,31 +1,12 @@
 using BookStore.Api.Data;
 using BookStore.Api.Endpoints;
-using BookStore.Api.Interfaces;
-using BookStore.Api.Repositories;
-using FluentValidation;
-using Microsoft.EntityFrameworkCore;
+using BookStore.Api.Extensions;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddValidatorsFromAssembly(typeof(Program).Assembly, includeInternalTypes: true);
-builder.Services.AddOpenApi();
-builder.Services.AddDbContext<BookStoreContext>(options =>
-{
-    var connectionString = builder.Configuration.GetConnectionString("BookStoreContext")
-        ?? throw new InvalidOperationException("Connection string 'BookStoreContext' not found.");
-    var serverVersion = ServerVersion.AutoDetect(connectionString);
-
-    options.UseMySql(connectionString, serverVersion);
-
-    if (builder.Environment.IsDevelopment())
-    {
-        options
-            .LogTo(Console.WriteLine, LogLevel.Information)
-            .EnableSensitiveDataLogging()
-            .EnableDetailedErrors();
-    }
-});
-builder.Services.AddSingleton<IBookRepository, InMemoryBooksRepository>();
+builder.Services
+    .AddDatabaseContext(builder.Configuration, builder.Environment)
+    .AddApplicationServices();
 
 var app = builder.Build();
 app.Services.InitializeDb();
