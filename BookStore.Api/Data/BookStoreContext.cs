5f798eae6b1e95a67608f9b032ee1075c99a08fa
Ver0018 <rogerpantilyowger@gmail.com>
2025-04-10 22:52:28 +0800

    updated bookstore context, added relations

--- BookStore.Api/Data/BookStoreContext.cs
+++ BookStore.Api/Data/BookStoreContext.cs
@@ -5,9 +5,35 @@ namespace BookStore.Api.Data;
 
 public class BookStoreContext : DbContext
 {
+    public DbSet<Book> Books { get; set; }
+    public DbSet<BookCategory> BookCategories { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Author> Authors { get; set; }
+
     public BookStoreContext(DbContextOptions<BookStoreContext> options) : base(options)
     {
     }
 
-    public DbSet<Book> Books { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Book>()
+            .HasOne(book => book.Author)
+            .WithMany(Book => Book.Books)
+            .HasForeignKey(book => book.AuthorId);
+
+        modelBuilder.Entity<BookCategory>()
+            .HasKey(bookCategory => new { bookCategory.BookId, bookCategory.CategoryId });
+
+        modelBuilder.Entity<BookCategory>()
+           .HasOne(bookCategory => bookCategory.Book)
+           .WithMany(book => book.BookCategories)
+           .HasForeignKey(bookCategory => bookCategory.BookId);
+
+        modelBuilder.Entity<BookCategory>()
+            .HasOne(bookCategory => bookCategory.Category)
+            .WithMany(category => category.BookCategories)
+            .HasForeignKey(bookCategory => bookCategory.CategoryId);
+    }
 }
